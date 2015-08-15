6c80391ba95e1d841dd6de74ab918ff0d432b903
QuinnLion <quinn@lionspaws.net>
2015-08-15 01:47:57 -0700

    Update cm.mk
    port to t8lte/v496

--- cm.mk
+++ cm.mk
@@ -1,16 +1,16 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-$(call inherit-product, device/lge/v410/full_v410.mk)
+$(call inherit-product, device/lge/v410/full_v496.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
-PRODUCT_NAME := cm_v410
+PRODUCT_NAME := cm_v496
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="v410" \
-    PRODUCT_NAME="e7att" \
-    BUILD_FINGERPRINT="lge/e7lte_att_us/e7lte:4.4.2/KOT49I.V41010d/V41010d.1406542706:user/release-keys" \
-    PRIVATE_BUILD_DESC="e7lte_att_us-user 4.4.2 KOT49I.V41010d V41010d.1406542706 release-keys"
+    PRODUCT_DEVICE="v496" \
+    PRODUCT_NAME="t8lte" \
+    BUILD_FINGERPRINT="lge/t8lte_tmo_us/t8lte:5.0.2/LRX22G/1511416257fb8:user/release-keys" \
+    PRIVATE_BUILD_DESC="t8lte_tmo_us-user 5.0.2 LRX22G.V49610a V49610a.1511416257fb8 release-keys"
